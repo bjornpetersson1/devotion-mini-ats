fd5905f4113a5a7f6e9f8ddb2e0a2d13ab5c9308
bjornpetersson1 <bjornpetersson1@live.se>
2026-03-09 23:43:59 +0100

    Removed a <h1>

--- app/candidates/page.tsx
+++ app/candidates/page.tsx
@@ -159,8 +159,6 @@ export default function ActiveCandidates() {
         />
       </div>
 
-      <h1>Candidates</h1>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {filteredCandidates.map((candidate) => {
           const job = jobMap[candidate.job_id];
